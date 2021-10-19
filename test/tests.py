6b175f2b6be23ba22e5af84680520daf2d1c47ff
Matthew Burkard <matthewjburkard@gmail.com>
2021-10-19 18:38:18 -0400

    test: add super basic unit tests

--- test/tests.py
+++ test/tests.py
@@ -1,15 +1,27 @@
 import json
 import os
+import unittest
 from pathlib import Path
 
 from openrpc.objects import OpenRPCObject
 
 from openrpcclientgenerator.client_factory import ClientFactory
 
-pwd = os.getcwd()
-openrpc_doc = OpenRPCObject(**json.loads(Path(f"{pwd}/openrpc.json").read_text()))
-cf = ClientFactory(f"{pwd}/../dist/generated", openrpc_doc)
 
-# cf.build_c_sharp_client()
-cf.build_python_client()
-# cf.build_typescript_client()
+class RPCTest(unittest.TestCase):
+    def __init__(self, *args) -> None:
+        pwd = os.getcwd()
+        openrpc_doc = OpenRPCObject(
+            **json.loads(Path(f"{pwd}/openrpc.json").read_text())
+        )
+        self.cf = ClientFactory(f"{pwd}/../dist/generated", openrpc_doc)
+        super(RPCTest, self).__init__(*args)
+
+    def test_csharp(self) -> None:
+        self.assertEqual(str, type(self.cf.build_c_sharp_client()))
+
+    def test_python(self) -> None:
+        self.assertEqual(str, type(self.cf.build_python_client()))
+
+    def test_typescript(self) -> None:
+        self.assertEqual(str, type(self.cf.build_typescript_client()))
