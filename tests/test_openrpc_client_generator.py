f458ce8fa1d103d81f1d0ea3ef05ac2182f3f6ea
Matthew Burkard <matthewjburkard@gmail.com>
2021-12-25 17:15:21 -0500

    test: add tests with no schemas

--- tests/test_openrpc_client_generator.py
+++ tests/test_openrpc_client_generator.py
@@ -13,20 +13,36 @@ from openrpcclientgenerator.client_factory import ClientFactory, Language
 class RPCTest(unittest.TestCase):
     def __init__(self, *args) -> None:
         pwd = os.getcwd()
-        openrpc_doc = OpenRPCObject(
-            **json.loads(Path(f"{pwd}/openrpc.json").read_text())
+        test_openrpc_doc = OpenRPCObject(
+            **json.loads(Path(f"{pwd}/test_openrpc.json").read_text())
         )
-        self.cf = ClientFactory(f"{pwd}/../dist/generated", openrpc_doc)
+        self.test_cf = ClientFactory(f"{pwd}/../dist/generated", test_openrpc_doc)
+        math_openrpc_doc = OpenRPCObject(
+            **json.loads(Path(f"{pwd}/math_openrpc.json").read_text())
+        )
+        self.math_cf = ClientFactory(f"{pwd}/../dist/generated", math_openrpc_doc)
         super(RPCTest, self).__init__(*args)
 
-    def test_csharp(self) -> None:
-        self.assertEqual(str, type(self.cf.generate_client(Language.DOTNET)))
+    def test_dotnet(self) -> None:
+        self.assertEqual(str, type(self.test_cf.generate_client(Language.DOTNET)))
 
     def test_kotlin(self) -> None:
-        self.assertEqual(str, type(self.cf.generate_client(Language.KOTLIN)))
+        self.assertEqual(str, type(self.test_cf.generate_client(Language.KOTLIN)))
 
     def test_python(self) -> None:
-        self.assertEqual(str, type(self.cf.generate_client(Language.PYTHON)))
+        self.assertEqual(str, type(self.test_cf.generate_client(Language.PYTHON)))
 
     def test_typescript(self) -> None:
-        self.assertEqual(str, type(self.cf.generate_client(Language.TYPE_SCRIPT)))
+        self.assertEqual(str, type(self.test_cf.generate_client(Language.TYPE_SCRIPT)))
+
+    def test_dotnet_no_models(self) -> None:
+        self.assertEqual(str, type(self.math_cf.generate_client(Language.DOTNET)))
+
+    def test_kotlin_no_models(self) -> None:
+        self.assertEqual(str, type(self.math_cf.generate_client(Language.KOTLIN)))
+
+    def test_python_no_models(self) -> None:
+        self.assertEqual(str, type(self.math_cf.generate_client(Language.PYTHON)))
+
+    def test_typescript_no_models(self) -> None:
+        self.assertEqual(str, type(self.math_cf.generate_client(Language.TYPE_SCRIPT)))
