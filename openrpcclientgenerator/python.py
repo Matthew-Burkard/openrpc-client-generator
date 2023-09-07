e11e9c95ebf01aee518930d5959cd1c349447256
Matthew Burkard <matthewjburkard@gmail.com>
2023-09-07 10:08:42 -0400

    feat: add python model generation

--- openrpcclientgenerator/python.py
+++ openrpcclientgenerator/python.py
@@ -21,23 +21,42 @@ type_map = {
 }
 
 
-def generate_client(rpc: OpenRPC, transport: str) -> None:
+def generate_client(rpc: OpenRPC, transport: str, out: Path) -> None:
     """Generate a Python client."""
+    out.mkdir(exist_ok=True)
+    client, models = _get_code(rpc, transport)
+    client_path = out.joinpath(caseswitcher.to_kebab(f"{rpc.info.title}-client"))
+    client_path.mkdir(exist_ok=True)
+    src_dir = client_path.joinpath(caseswitcher.to_snake(f"{rpc.info.title}_client"))
+    src_dir.mkdir(exist_ok=True)
+    client_file = src_dir.joinpath("client.py")
+    client_file.touch(exist_ok=True)
+    client_file.write_text(client)
+    models_file = src_dir.joinpath("models.py")
+    models_file.touch(exist_ok=True)
+    models_file.write_text(models)
+
+
+def _get_code(rpc: OpenRPC, transport: str) -> tuple[str, str]:
     group = common.get_rpc_group(caseswitcher.to_pascal(rpc.info.title), rpc.methods)
     env = Environment(
         loader=FileSystemLoader(templates), lstrip_blocks=True, trim_blocks=True
     )
-    template = env.get_template("python/client_module.j2")
-    context = {
-        "transport": transport,
-        "group": group,
-        "indent": "",
-        "py_type": py_type,
-    }
-    rendered_template = template.render(context)
-    print(
-        black.format_str(rendered_template, mode=black.Mode(magic_trailing_comma=False))
+    client_template = env.get_template("python/client_module.j2")
+    context = {"transport": transport, "group": group, "indent": "", "py_type": py_type}
+    client = black.format_str(
+        client_template.render(context), mode=black.Mode(magic_trailing_comma=False)
     )
+    context = {
+        "schemas": rpc.components.schemas,
+        "py_type": py_type,
+        "cs": caseswitcher,
+    }
+    models_template = env.get_template("python/models.j2")
+    models = black.format_str(
+        models_template.render(context), mode=black.Mode(magic_trailing_comma=False)
+    )
+    return client, models
 
 
 def py_type(schema: Schema) -> str:
@@ -58,7 +77,7 @@ def py_type(schema: Schema) -> str:
         return type_map[schema.type]
     elif schema_list := schema.all_of or schema.any_of or schema.one_of:
         str_types = [py_type(it) for it in schema_list]
-        return " | ".join(type_map[it] for it in str_types)
+        return " | ".join(str_types)
     elif schema.ref:
         return re.sub(r"#/.*/(.*)", r"\1", schema.ref)
 
